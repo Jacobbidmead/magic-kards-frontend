682bf4def537849d3e224cc63e585b8755f09dd5
hamuuri <mohammed_125@outlook.com>
2022-10-05 11:45:31 +0700

    added a character select page (needs to be adjusted)

---
+++ index.js
@@ -0,0 +1,7 @@
+const mario() {
+  document.getElementById("character-box").style.backgroundImage = "http://www.heromachine.com/wp-content/uploads/2015/10/141-ams-It-Comes-from-FIRE.png"
+
+}
+const ice() {
+  document.getElementById("character-box").style.backgroundImage = "https://i.pinimg.com/236x/34/d9/95/34d9956d1a84783aa13f7c1c70b52f6c.jpg";
+}
